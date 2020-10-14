1f3517399d692232e188d94a7b0860778e46ee25
Coder Matt <49959920+MathieuDonofrio@users.noreply.github.com>
2020-10-13 22:59:42 -0400

    Added more response data to internship application services & Unit tests

--- src/main/java/cal/internshipmanager/service/InternshipApplicationService.java
+++ src/main/java/cal/internshipmanager/service/InternshipApplicationService.java
@@ -3,7 +3,9 @@ package cal.internshipmanager.service;
 import cal.internshipmanager.model.InternshipApplication;
 import cal.internshipmanager.model.PortfolioDocument;
 import cal.internshipmanager.repository.InternshipApplicationRepository;
+import cal.internshipmanager.repository.InternshipOfferRepository;
 import cal.internshipmanager.repository.PortfolioDocumentRepository;
+import cal.internshipmanager.repository.UserRepository;
 import cal.internshipmanager.request.InternshipApplicationCreationRequest;
 import cal.internshipmanager.request.InternshipApplicationEditRequest;
 import cal.internshipmanager.response.InternshipApplicationListResponse;
@@ -29,14 +31,22 @@ public class InternshipApplicationService {
 
     private final PortfolioDocumentRepository portfolioDocumentRepository;
 
+    private final UserRepository userRepository;
+
+    private final InternshipOfferRepository internshipOfferRepository;
+
     //
     // Constructors
     //
 
     public InternshipApplicationService(InternshipApplicationRepository internshipApplicationRepository,
-                                        PortfolioDocumentRepository portfolioDocumentRepository) {
+                                        PortfolioDocumentRepository portfolioDocumentRepository,
+                                        UserRepository userRepository,
+                                        InternshipOfferRepository internshipOfferRepository) {
         this.internshipApplicationRepository = internshipApplicationRepository;
         this.portfolioDocumentRepository = portfolioDocumentRepository;
+        this.userRepository = userRepository;
+        this.internshipOfferRepository = internshipOfferRepository;
     }
 
     //
@@ -74,7 +84,8 @@ public class InternshipApplicationService {
         InternshipApplicationListResponse response = new InternshipApplicationListResponse();
 
         response.setApplications(userApplications.stream()
-                .map(x -> InternshipApplicationListResponse.map(x)).collect(Collectors.toList()));
+                .map(x -> InternshipApplicationListResponse.map(userRepository, internshipOfferRepository, x))
+                .collect(Collectors.toList()));
 
         return response;
     }
@@ -86,7 +97,7 @@ public class InternshipApplicationService {
         InternshipApplicationListResponse response = new InternshipApplicationListResponse();
 
         response.setApplications(allApplications.stream()
-                .map(application -> InternshipApplicationListResponse.map(application))
+                .map(x -> InternshipApplicationListResponse.map(userRepository, internshipOfferRepository, x))
                 .collect(Collectors.toList()));
 
         return response;
