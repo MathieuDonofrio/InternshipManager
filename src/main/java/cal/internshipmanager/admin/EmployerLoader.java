ce493eb42a6a269557852afd052bfcb96db0b799
Coder Matt <49959920+MathieuDonofrio@users.noreply.github.com>
2020-10-13 21:46:27 -0400

    Refractor Data loaders (Now using faker)

--- src/main/java/cal/internshipmanager/admin/EmployerLoader.java
+++ src/main/java/cal/internshipmanager/admin/EmployerLoader.java
@@ -2,6 +2,7 @@ package cal.internshipmanager.admin;
 
 import cal.internshipmanager.model.User;
 import cal.internshipmanager.repository.UserRepository;
+import com.github.javafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
@@ -15,14 +16,20 @@ import java.util.UUID;
 public class EmployerLoader implements CommandLineRunner {
 
     //
+    // Constants
+    //
+
+    private static final int MIN_EMPLOYER_AMOUNT = 8;
+
+    private static final String PASSWORD = "123456";
+
+    //
     // Dependencies
     //
 
     private final UserRepository userRepository;
 
     private final PasswordEncoder passwordEncoder;
-
-    public static final int EMPLOYER_COUNT = 6;
 
     //
     // Constructors
@@ -40,23 +47,29 @@ public class EmployerLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        LoadEveryoneIfAbsent();
+
+        final int needed = MIN_EMPLOYER_AMOUNT -  userRepository.findAllByType("EMPLOYER").size();
+
+        for(int i = 0; i < needed; i++){
+
+            final User user = generate();
+
+            userRepository.save(user);
+        }
     }
 
     //
     // Private Methods
     //
 
-    private void LoadEveryoneIfAbsent() {
-        if (userRepository.findAllByType("EMPLOYER").isEmpty()) {
-            for (int i = 0; i < EMPLOYER_COUNT; i++) {
-                Load("employer" + i + "@employer.com", "employer", "employer", "123456");
-            }
-        }
-    }
+    private User generate() {
 
+        final Faker faker = new Faker();
 
-    private void Load(String email, String firstName, String lastName, String password) {
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+
+        String email = firstName + "_" + lastName + "@faker.com";
 
         User user = new User();
 
@@ -65,9 +78,9 @@ public class EmployerLoader implements CommandLineRunner {
         user.setEmail(email);
         user.setFirstName(firstName);
         user.setLastName(lastName);
-        user.setPasswordHash(passwordEncoder.encode(password));
+        user.setPasswordHash(passwordEncoder.encode(PASSWORD));
 
-        userRepository.save(user);
+        return user;
     }
 
 }
