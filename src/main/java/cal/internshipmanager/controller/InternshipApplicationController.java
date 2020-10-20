3a852b88d2e28d914dd51ddf9684129aec825b8a
progdev8201 <56039386+progdev8201@users.noreply.github.com>
2020-10-20 13:07:19 -0400

    Update InternshipApplicationController.java

--- src/main/java/cal/internshipmanager/controller/InternshipApplicationController.java
+++ src/main/java/cal/internshipmanager/controller/InternshipApplicationController.java
@@ -9,7 +9,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/internship-application")
@@ -33,11 +36,7 @@ public class InternshipApplicationController {
     // Services
     //
 
-    @PreAuthorize("hasAuthority('STUDENT')")
-    @PostMapping("create")
-    public void create(@Valid @RequestBody InternshipApplicationCreationRequest request) {
-        internshipApplicationService.create(request);
-    }
+    // GET
 
     @GetMapping("internship-applications/{userUniqueId}")
     public InternshipApplicationListResponse internshipApplications(@PathVariable UUID userUniqueId) {
@@ -49,9 +48,25 @@ public class InternshipApplicationController {
         return internshipApplicationService.findByStatus(status);
     }
 
+    @PreAuthorize("hasAuthority('EMPLOYER')")
+    @GetMapping("offer/{uniqueId}")
+    public InternshipApplicationListResponse findByOffer(@PathVariable UUID offerUniqueId) {
+        return internshipApplicationService.findByOffer(offerUniqueId);
+    }
+
+    //PUT
+
     @PutMapping
     public void edit(@RequestBody @Valid InternshipApplicationEditRequest request) {
         internshipApplicationService.editStatus(request);
+    }
+
+    //POST
+
+    @PreAuthorize("hasAuthority('STUDENT')")
+    @PostMapping("create")
+    public void create(@Valid @RequestBody InternshipApplicationCreationRequest request) {
+        internshipApplicationService.create(request);
     }
 
 }
