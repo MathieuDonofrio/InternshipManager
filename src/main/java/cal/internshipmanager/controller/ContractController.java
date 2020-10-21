fa7d6407598ab4b958e355f10dba39015ebc9a0a
Coder Matt <49959920+MathieuDonofrio@users.noreply.github.com>
2020-10-21 01:07:37 -0400

    Proof of work contract generation

---
+++ src/main/java/cal/internshipmanager/controller/ContractController.java
@@ -0,0 +1,56 @@
+package cal.internshipmanager.controller;
+
+import cal.internshipmanager.model.PortfolioDocument;
+import cal.internshipmanager.service.AuthenticationService;
+import cal.internshipmanager.service.ContractService;
+import cal.internshipmanager.validator.ExistingInternshipApplication;
+import cal.internshipmanager.validator.ExistingPortfolioDocument;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@RestController("/contract")
+public class ContractController {
+
+    //
+    // Dependencies
+    //
+
+    private final ContractService contractService;
+
+    //
+    // Constructors
+    //
+
+    @Autowired
+    public ContractController(ContractService contractService) {
+        this.contractService = contractService;
+    }
+
+    //
+    // Get
+    //
+
+    @GetMapping("internship-application/{uniqueId}")
+    public ResponseEntity<byte[]> download(@Valid @PathVariable @ExistingInternshipApplication UUID uniqueId) {
+
+        byte[] data = Base64Utils.encode(contractService.generateContract(uniqueId));
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.parseMediaType("pdf"));
+        headers.setContentLength(data.length);
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + "Contrat" + "\"");
+
+        return ResponseEntity.ok().headers(headers).body(data);
+    }
+
+}
--- src/main/java/cal/internshipmanager/service/ContractService.java
+++ src/main/java/cal/internshipmanager/service/ContractService.java
@@ -7,12 +7,22 @@ import cal.internshipmanager.repository.PortfolioDocumentRepository;
 import cal.internshipmanager.repository.UserRepository;
 import cal.internshipmanager.security.JwtProvider;
 import cal.internshipmanager.validator.ExistingInternshipApplication;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Header;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfDocument;
+import com.itextpdf.text.pdf.PdfWriter;
+import lombok.SneakyThrows;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
+import java.io.ByteArrayOutputStream;
+import java.util.UUID;
 
 @Service
 @Validated
@@ -45,7 +55,23 @@ public class ContractService {
     // Services
     //
 
-    public void GenerateContract(@Valid @ExistingInternshipApplication InternshipApplication applicationUniqueId){
-        //TODO
+    @SneakyThrows
+    public byte[] generateContract(@Valid @ExistingInternshipApplication UUID uniqueId) {
+
+        InternshipApplication application = internshipApplicationRepository.findById(uniqueId).orElse(null);
+
+        PdfDocument pdfdoc = new PdfDocument();
+
+        pdfdoc.add(new Paragraph("Contrat (Proof of work)"));
+        pdfdoc.add(new Paragraph("Application UUID: " + uniqueId));
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+        PdfWriter.getInstance(pdfdoc, stream);
+
+        pdfdoc.open();
+        pdfdoc.close();
+
+        return stream.toByteArray();
     }
 }
