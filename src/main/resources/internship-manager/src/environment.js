280a3a536fe091713ee21cafa959d15da86f404b
Hichem <hichemfredj123@gmail.com>
2020-11-01 17:55:52 -0500

    change link that point to the back-end

--- src/main/resources/internship-manager/src/environment.js
+++ src/main/resources/internship-manager/src/environment.js
@@ -1,4 +1,5 @@
 
 export const Config={
-    target: 'http://localhost:9090'
+    //target: 'http://localhost:9090',
+    target: 'InternshipManager-env.eba-sutpjk8m.us-east-2.elasticbeanstalk.com'
 }
