846cf37dfd5a22430a7a73bf916983ec85de1aa0
Hichem <hichemfredj123@gmail.com>
2020-11-14 15:38:28 -0500

    Configuration change

--- src/main/java/cal/internshipmanager/configuration/SecurityConfiguration.java
+++ src/main/java/cal/internshipmanager/configuration/SecurityConfiguration.java
@@ -66,9 +66,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
+       // configuration.setAllowedOrigins(Collections.singletonList("http://react-intershipmanager.s3-website.us-east-2.amazonaws.com"));
 
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
 
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); configuration.setAllowedOrigins(Collections.singletonList("http://react-intershipmanager.s3-website.us-east-2.amazonaws.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
 
         configuration.addAllowedHeader("Content-Type");
         configuration.addAllowedHeader("Access-Control-Allow-Origin");
--- src/main/resources/internship-manager/src/environment.js
+++ src/main/resources/internship-manager/src/environment.js
@@ -1,5 +1,5 @@
 
 export const Config={
-    //target: 'http://localhost:5000',
-    target: 'http://internshipManager-env.eba-sutpjk8m.us-east-2.elasticbeanstalk.com'
+    target: 'http://localhost:9090',
+    //target: 'http://internshipManager-env.eba-sutpjk8m.us-east-2.elasticbeanstalk.com'
 }
