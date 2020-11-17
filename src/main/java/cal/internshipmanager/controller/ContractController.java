174e13defdd2b604ad143c86704e1a32882a453c
Coder Matt <49959920+MathieuDonofrio@users.noreply.github.com>
2020-11-17 00:00:10 -0500

    - Contract Repository - Contract Services - Contract Controller - Validators

--- src/main/java/cal/internshipmanager/controller/ContractController.java
+++ src/main/java/cal/internshipmanager/controller/ContractController.java
@@ -1,17 +1,18 @@
 package cal.internshipmanager.controller;
 
+import cal.internshipmanager.response.ContractListResponse;
 import cal.internshipmanager.response.DownloadFileResponse;
 import cal.internshipmanager.service.ContractService;
+import cal.internshipmanager.validator.ExistingContract;
 import cal.internshipmanager.validator.ExistingInternshipApplication;
+import cal.internshipmanager.validator.ExistingUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 @RestController
@@ -37,9 +38,24 @@ public class ContractController {
     // Get
     //
 
-    @GetMapping("internship-application/{uniqueId}")
+    @GetMapping("awaiting-signature/{userUniqueId}")
+    public ContractListResponse awaitingSignature(@Valid @PathVariable @ExistingUser UUID userUniqueId){
+        return contractService.awaitingSignature(userUniqueId);
+    }
+
+    @GetMapping("internship-application/{uniqueId}") // TODO replace internship-application by generate
     public ResponseEntity<Resource> generate(@Valid @PathVariable @ExistingInternshipApplication UUID uniqueId) {
         return DownloadFileResponse.responseEntity(contractService.generate(uniqueId));
     }
+
+    //
+    // Put
+    //
+
+    @PutMapping("sign/{uniqueId}")
+    public void semester(@Valid @PathVariable @ExistingContract UUID uniqueId) {
+        contractService.sign(uniqueId);
+    }
+
 
 }
